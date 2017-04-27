b1e94ea6425a8116e8dd07eac28867a3ae3bbc76
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-04-27 23:46:09 +0200

    working but needs bugfixing

--- memory-game.py
+++ memory-game.py
@@ -11,17 +11,15 @@ animals = random.sample(animals, len(animals))
 animals_dict = {}
 for index, animal in enumerate(animals):
     animals_dict[index] = animal
-# print(animals_dict)
+print(animals_dict)
 table = []
 for i in range(len(animals)):
     table.append('0')
 rows = int(len(animals) ** 0.5)
-# print_table(table)
 first_guess = True
 found = 0
 
 
-'''
 def print_table(table_to_print):
     rows = int(len(animals) ** 0.5)
     for row in range(rows):
@@ -29,15 +27,28 @@ def print_table(table_to_print):
             print(table_to_print[rows*row+cell], ' ', end='')
         print()
     return
-'''
 
-'''
-while found < int(len(animals)/2):
-    guess = -1
-    while guess < 0 or guess > len(table)-1 or table[guess] != '-' or (not first_guess and guess == first):
-        guess = int(input('Next turn: '))
+
+@app.route('/', methods=['GET', 'POST'])
+def list():
+    if request.method == "POST":
+        return add_new_story()
+    else:
+        return render_template('table.html', table=table, rows=rows, found=found)
+
+
+@app.route('/click/<id>')
+def click(id):
+    print(id)
+    global first_guess
+    global found
+    global temp_table
+    global first_guessed_animal
+    global second_guessed_animal
+    if found == 8:
+        return redirect('/')
     if first_guess:
-        first = guess
+        first = int(id)
         first_guessed_animal = animals_dict.get(first)
         temp_table = []
         for i in range(len(table)):
@@ -45,11 +56,11 @@ while found < int(len(animals)/2):
                 temp_table.append(first_guessed_animal)
             else:
                 temp_table.append(table[i])
-        print_table(temp_table)
         first_guess = False
-        continue
+        print_table(temp_table)
+        return render_template('table.html', table=temp_table, rows=rows, found=found)
     else:
-        second = guess
+        second = int(id)
         second_guessed_animal = animals_dict.get(second)
         for i in range(len(temp_table)):
             if i == second:
@@ -61,23 +72,16 @@ while found < int(len(animals)/2):
                     table[i] = temp_table[i]
                 print('Nice guess! ')
                 first_guess = True
-                continue
+                print_table(temp_table)
+                return render_template('table.html', table=temp_table, rows=rows, found=found)
         else:
-                input('Wrong! Press any key to try again! ')
+                print('Wrong! Press any key to try again! ')
                 print_table(table)
                 first_guess = True
-                continue
-print('You won!')
-'''
+                print_table(temp_table)
+                return render_template('table.html', table=temp_table, rows=rows, found=found)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def list():
-    if request.method == "POST":
-        return add_new_story()
-    else:
-        return render_template('table.html', table=table, rows=rows)
-
-
+print_table(table)
 if __name__ == '__main__':
     app.run()
